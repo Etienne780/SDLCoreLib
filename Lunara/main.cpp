470a21ce9426695a5f936de45b449802624cf72f
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2025-11-02 20:33:53 +0100

    feat: Create Lunara

--- Lunara/main.cpp
+++ Lunara/main.cpp
@@ -1,8 +1,13 @@
 #include <CoreLib/Log.h>
-#include <iostream>
+#include <SDLCoreLib/Application.h>
+
+#include "Lunara.h"
 
 int main() {
-	
+	Lunara lunara;
 
+	SDLCore::SDLResult result = lunara.Start();
+	Log::Info(SDLCore::GetError(result));
+	
 	return 0;
 }
