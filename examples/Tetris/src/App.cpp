91c4c55abe25e8629c0d3be63da0f96ad62f104e
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2025-12-06 19:32:23 +0100

    fix: not important

--- examples/Tetris/src/App.cpp
+++ examples/Tetris/src/App.cpp
@@ -34,54 +34,15 @@ void App::OnUpdate() {
     }
 
     /*
-// SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay())
-UI::SetWindowScale()
+    using namespace SDLCore::UI;
 
-fontNumberKey = UI::RegisterFontSize(fontSize);
-UI::ActivateFontSize(fontNumberKey);
-UI::SetFontSize();
-
-colorNumberKey = UI::RegisterColor(color);
-UI::ActivateBackgroundColor(colorNumberKey);
-UI::ActivateBorderColor(colorNumberKey);
-UI::ActivateTintColor(colorNumberKey);
-UI::ActivateTextColor(colorNumberKey);
-UI::SetBackgroundColor(Color);
-UI::SetBorderColor(Color);
-UI::SetTintColor(Color);
-UI::SetTextColor(Color);
-
-imageNumberKey = UI::RegisterImage(image);
-
-UI::SetNextMargin(value, value, value, value)
-UI::SetNextPadding(value, value, value, value)
-
-UI::SetNextSize()
-UI::SetNextPosition()
-
-UI::SetNextTypeRelative()
-- if a ui is static it 0,0 will be the position of this element
-    like in css Position: relative
-
-UI::SetNextUIType(static, dynamic)
-- static: UI has a static position, like in css Position: absolute
-- dynamic: UI has a dynamic position with anchor points
-
-UI::SetNextAnchorPoint(Ver: START/CENTER/END, Hor: START/CENTER/END)
-
-UI::GetPosition()
-UI::GetSize()
-UI::GetTypeRelative()
-UI::GetUIType()
-UI::GetAnchorVer()
-UI::GetAnchorHor()
-
-UI::Begin(imageNumberKey);
-{
-    UI::Text("text");
-}
-UI::End();
-*/
+    BeginFrame();
+    {   
+        
+    }
+    EndFrame();
+    
+    */
 }
 
 void App::OnQuit() {
