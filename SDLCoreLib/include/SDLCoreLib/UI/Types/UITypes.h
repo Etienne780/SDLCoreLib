7cf61c5d3a367529a31336a0879617f3c2a526c9
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2025-12-07 16:39:51 +0100

    feat: UI Types

---
+++ SDLCoreLib/include/SDLCoreLib/UI/Types/UITypes.h
@@ -0,0 +1,181 @@
+#pragma once
+#include <variant>
+
+#include <CoreLib/Math/Vector2.h>
+#include <CoreLib/Math/Vector4.h>
+#include "Types/Texture.h"
+#include "Types/Font/Font.h"
+#include "Types/Types.h"
+
+namespace SDLCore::UI {
+	
+	enum class State {
+		NORMAL = 0,
+		HOVER,
+		ACTIVE,
+		CLICK,
+		DISABLED
+	};
+
+	enum class Alignment {
+		START = 0,
+		CENTER,
+		END
+	};
+
+	enum class LayoutDirection {
+		ROW = 0,// is equal to using ROW_START
+		COLUMN,// is equal to using COLUMN_START
+		ROW_START,
+		ROW_END,
+		COLUMN_START, 
+		COLUMN_END
+	};
+	using LayoutDir = LayoutDirection;
+
+	enum class SizeUnit {
+		PX = 0,
+		PERCENTAGE,
+		PERCENTAGE_W,
+		PERCENTAGE_H
+	};
+
+	struct UIColorTag {};
+	struct UIFontTag {};
+	struct UITextureTag {};
+	struct UINumberTag {};
+
+	/**
+	* @brief Identifier for a color (used by the UIStyles).
+	*        Internally stored as an uint32_t
+	*/
+	using UIColorID = SDLCoreID<UIColorTag>;
+
+	/**
+	* @brief Identifier for a font (used by the UIStyles).
+	*        Internally stored as an uint32_t
+	*/
+	using UIFontID = SDLCoreID<UIFontTag>;
+
+	/**
+	* @brief Identifier for a texture (used by the UIStyles).
+	*        Internally stored as an uint32_t
+	*/
+	using UITextureID = SDLCoreID<UITextureTag>;
+
+	/**
+	* @brief Identifier for a texture (used by the UIStyles).
+	*        Internally stored as an uint32_t
+	*/
+	using UINumberID = SDLCoreID<UINumberTag>;
+
+	class PropertyValue {
+	public:
+		enum class Type {
+			INT, FLOAT, DOUBLE, VECTOR2, VECTOR4, TEXTURE, FONT, COLOR_ID, FONT_ID, TEXTURE_ID, NUMBER_ID
+		};
+
+		PropertyValue();
+		PropertyValue(int i);
+		PropertyValue(float f);
+		PropertyValue(double d);
+		PropertyValue(const Vector2& vec);
+		PropertyValue(const Vector4& vec);
+		PropertyValue(const Texture& tex);
+		PropertyValue(std::shared_ptr<Font> font);
+		PropertyValue(UIColorID id);
+		PropertyValue(UIFontID id);
+		PropertyValue(UITextureID id);
+		PropertyValue(UINumberID id);
+
+		Type GetType() const;
+		bool GetIsSet() const;
+		bool GetIsImportant() const;
+
+		PropertyValue& SetIsSet(bool value);
+		PropertyValue& SetIsImportant(bool value);
+
+		PropertyValue& SetValue(int i);
+		PropertyValue& SetValue(float f);
+		PropertyValue& SetValue(double d);
+		PropertyValue& SetValue(const Vector2& vec);
+		PropertyValue& SetValue(const Vector4& vec);
+		PropertyValue& SetValue(const Texture& tex);
+		PropertyValue& SetValue(std::shared_ptr<Font> font);
+		PropertyValue& SetValue(UIColorID id);
+		PropertyValue& SetValue(UIFontID id);
+		PropertyValue& SetValue(UITextureID id);
+		PropertyValue& SetValue(UINumberID id);
+
+		/**
+		* @brief Attempts to retrieve the stored value as a specific type.
+		* @tparam T Expected value type.
+		* @param out Output reference to receive the value.
+		* @return True if successful, false if type mismatch.
+		*/
+		template<typename T>
+		bool TryGetValue(T& out) const {
+			if (std::holds_alternative<T>(m_value)) {
+				out = std::get<T>(m_value);
+				return true;
+			}
+
+			Log::Warn("SDLCore::UI::PropertyValue: Could not get value wrong data type, expected type '{}', got '{}'!",
+				GetReadableTypeName(typeid(T)), m_valueType);
+			return false;
+		}
+
+	private:
+		using ValueVariant = std::variant<int, float, double, Vector2, Vector4, Texture, std::shared_ptr<Font>, UIColorID, UIFontID, UITextureID, UINumberID>;
+		ValueVariant m_value;
+		Type m_valueType = Type::INT;
+		bool m_isSet = false;
+		bool m_isImportant = false;
+
+		/**
+		* @brief Helper function that returns a readable type name for type_info.
+		* @param type Type information.
+		* @return std::string representing the readable type name.
+		*/
+		static std::string GetReadableTypeName(const std::type_info& type);
+	};
+
+}
+
+template<>
+static inline std::string FormatUtils::toString<SDLCore::SDLCoreID<SDLCore::UI::UIColorTag>>(SDLCore::UI::UIColorID id) {
+	return id.ToString();
+}
+
+template<>
+static inline std::string FormatUtils::toString<SDLCore::SDLCoreID<SDLCore::UI::UIFontTag>>(SDLCore::UI::UIFontID id) {
+	return id.ToString();
+}
+
+template<>
+static inline std::string FormatUtils::toString<SDLCore::SDLCoreID<SDLCore::UI::UITextureTag>>(SDLCore::UI::UITextureID id) {
+	return id.ToString();
+}
+
+template<>
+static inline std::string FormatUtils::toString<SDLCore::SDLCoreID<SDLCore::UI::UINumberTag>>(SDLCore::UI::UINumberID id) {
+	return id.ToString();
+}
+
+template<>
+static inline std::string FormatUtils::toString<SDLCore::UI::PropertyValue::Type>(SDLCore::UI::PropertyValue::Type type) {
+	switch (type) {
+	case SDLCore::UI::PropertyValue::Type::INT: return "int";
+	case SDLCore::UI::PropertyValue::Type::FLOAT: return "float";
+	case SDLCore::UI::PropertyValue::Type::DOUBLE: return "double";
+	case SDLCore::UI::PropertyValue::Type::VECTOR2: return "Vector2";
+	case SDLCore::UI::PropertyValue::Type::VECTOR4: return "Vector4";
+	case SDLCore::UI::PropertyValue::Type::TEXTURE: return "Texture";
+	case SDLCore::UI::PropertyValue::Type::FONT: return "Font";
+	case SDLCore::UI::PropertyValue::Type::COLOR_ID: return "UIColorID";
+	case SDLCore::UI::PropertyValue::Type::FONT_ID: return "UIFontID";
+	case SDLCore::UI::PropertyValue::Type::TEXTURE_ID: return "UITextureID";
+	case SDLCore::UI::PropertyValue::Type::NUMBER_ID: return "UINumberID";
+	default: return "UNKNOWN";
+	}
+}
---
+++ SDLCoreLib/src/SDLCoreLib/UI/Types/UITypes.cpp
@@ -0,0 +1,159 @@
+#include "UI/Types/UITypes.h"
+
+namespace SDLCore::UI {
+
+	#pragma region PropertyValue
+
+	PropertyValue::PropertyValue() 
+		: m_value(0), m_valueType(Type::INT) {
+	}
+
+	PropertyValue::PropertyValue(int i)
+		: m_value(i), m_valueType(Type::INT) {
+	}
+
+	PropertyValue::PropertyValue(float f)
+		: m_value(f), m_valueType(Type::FLOAT) {
+	}
+
+	PropertyValue::PropertyValue(double d) 
+		: m_value(d), m_valueType(Type::DOUBLE) {
+	}
+
+	PropertyValue::PropertyValue(const Vector2& vec)
+		: m_value(vec), m_valueType(Type::VECTOR2) {
+	}
+
+	PropertyValue::PropertyValue(const Vector4& vec)
+		: m_value(vec), m_valueType(Type::VECTOR4) {
+	}
+
+	PropertyValue::PropertyValue(const Texture& tex)
+		: m_value(tex), m_valueType(Type::TEXTURE) {
+	}
+
+	PropertyValue::PropertyValue(std::shared_ptr<Font> font)
+		: m_value(font), m_valueType(Type::FONT) {
+	}
+
+	PropertyValue::PropertyValue(UIColorID id)
+		: m_value(id), m_valueType(Type::COLOR_ID) {
+	}
+
+	PropertyValue::PropertyValue(UIFontID id)
+		: m_value(id), m_valueType(Type::FONT_ID) {
+	}
+
+	PropertyValue::PropertyValue(UITextureID id)
+		: m_value(id), m_valueType(Type::TEXTURE_ID) {
+	}
+
+	PropertyValue::PropertyValue(UINumberID id)
+		: m_value(id), m_valueType(Type::NUMBER_ID) {
+	}
+
+	PropertyValue::Type PropertyValue::GetType() const {
+		return m_valueType;
+	}
+
+	bool PropertyValue::GetIsSet() const {
+		return m_isSet;
+	}
+
+	bool PropertyValue::GetIsImportant() const {
+		return m_isImportant;
+	}
+
+	PropertyValue& PropertyValue::SetIsSet(bool value) {
+		m_isSet = value;
+		return *this;
+	}
+
+	PropertyValue& PropertyValue::SetIsImportant(bool value) {
+		m_isImportant = value;
+		return *this;
+	}
+
+	PropertyValue& PropertyValue::SetValue(int i) {
+		m_valueType = Type::INT;
+		m_value = i;
+		return *this;
+	}
+
+	PropertyValue& PropertyValue::SetValue(float f) {
+		m_valueType = Type::FLOAT;
+		m_value = f;
+		return *this;
+	}
+
+	PropertyValue& PropertyValue::SetValue(double d) {
+		m_valueType = Type::DOUBLE;
+		m_value = d;
+		return *this;
+	}
+
+	PropertyValue& PropertyValue::SetValue(const Vector2& vec) {
+		m_valueType = Type::VECTOR2;
+		m_value = vec;
+		return *this;
+	}
+
+	PropertyValue& PropertyValue::SetValue(const Vector4& vec) {
+		m_valueType = Type::VECTOR4;
+		m_value = vec;
+		return *this;
+	}
+
+	PropertyValue& PropertyValue::SetValue(const Texture& tex) {
+		m_valueType = Type::TEXTURE;
+		m_value = tex;
+		return *this;
+	}
+
+	PropertyValue& PropertyValue::SetValue(std::shared_ptr<Font> font) {
+		m_valueType = Type::FONT;
+		m_value = font;
+		return *this;
+	}
+
+	PropertyValue& PropertyValue::SetValue(UIColorID id) {
+		m_valueType = Type::COLOR_ID;
+		m_value = id;
+		return *this;
+	}
+
+	PropertyValue& PropertyValue::SetValue(UIFontID id) {
+		m_valueType = Type::FONT_ID;
+		m_value = id;
+		return *this;
+	}
+
+	PropertyValue& PropertyValue::SetValue(UITextureID id) {
+		m_valueType = Type::TEXTURE_ID;
+		m_value = id;
+		return *this;
+	}
+
+	PropertyValue& PropertyValue::SetValue(UINumberID id) {
+		m_valueType = Type::NUMBER_ID;
+		m_value = id;
+		return *this;
+	}
+
+	std::string PropertyValue::GetReadableTypeName(const std::type_info& type) {
+		if (type == typeid(int)) return "int";
+		if (type == typeid(float)) return "float";
+		if (type == typeid(Vector2)) return "Vector2";
+		if (type == typeid(Vector4)) return "Vector4";
+		if (type == typeid(Texture)) return "Texture";
+		if (type == typeid(std::shared_ptr<Font>)) return "Font";
+		if (type == typeid(UIColorID)) return "UIColorID";
+		if (type == typeid(UIFontID)) return "UIFontID";
+		if (type == typeid(UITextureID)) return "UITextureID";
+		if (type == typeid(UINumberID)) return "UINumberID";
+		return "UNKOWN";
+	}
+
+	#pragma endregion
+
+}
