f350a68b8d2cde7feab7c02982482d11184c6f40
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2025-12-11 21:50:22 +0100

    fix: Stack gets created/updated correct

--- examples/Tetris/src/App.cpp
+++ examples/Tetris/src/App.cpp
@@ -10,27 +10,55 @@ void App::OnStart() {
     SDLCore::Texture tex("J:/images/image.png");
     auto* win = CreateWindow(&m_winID, "Tetris", 800, 800);
     win->SetIcon(tex);
-
-    {
-        using namespace SDLCore;
-        UI::SetContextWindow(context, m_winID);
-
-        UI::UIStyle style("Test");
-        style.SetSize(10, 10);
-
-        UI::FrameScope root;
-        {
-            UI::FrameScope(UI::UIKey("test"), style);
-            {
-                
-            }
-        }
-    }
-
-    int i = 1;
 }
 
 void App::OnUpdate() {
+
+    {
+        static int test = 0;
+
+        using namespace SDLCore;
+        UI::SetContextWindow(context, m_winID);
+        UI::BindContext(context);
+
+        UI::UIStyle style("Test-Style");
+        style.SetSize(10, 10);
+
+        if (test == 3) {
+            Log::Debug("New Element middle test");
+        }
+        else if (test == 5) {
+            Log::Debug("Delete Element");
+        }
+
+        UI::BeginFrame(UI::UIKey("root"));
+        {
+            UI::BeginFrame(UI::UIKey("panel_1"), style);
+            {
+                UI::BeginFrame(UI::UIKey("panel_1_1"), style);
+                UI::EndFrame();
+            }
+            UI::EndFrame();
+
+            if (test >= 3) {
+                UI::BeginFrame(UI::UIKey("new element"));
+                UI::EndFrame();
+            }
+
+            if (test < 5) {
+                UI::BeginFrame(UI::UIKey("panel_2"), style);
+                UI::EndFrame();
+            }
+        }
+        UI::EndFrame();
+
+        Log::Print("Test {}", test);
+        Log::Print(UI::GetContextStringHierarchy(context));
+
+        test++;
+    }
+
+    /*
     // Quit if no windows remain
     if (GetWindowCount() <= 0)
         Quit();
@@ -48,10 +76,7 @@ void App::OnUpdate() {
 
         if (Input::KeyJustPressed(KeyCode::ESCAPE))
             DeleteWindow(m_winID);
-
-        if (Time::GetFrameCount() % 200 == 0)
-            Log::Print("FPS: {}", Time::GetFrameRateHzF());
-    }
+    }*/
 }
 
 void App::OnQuit() {
