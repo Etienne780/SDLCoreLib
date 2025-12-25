ab0f9b5b525f07028c6367d8002b08f141891ab2
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2025-12-25 14:01:49 +0100

    fix: performance upgrade

--- SDLCoreLib/include/SDLCoreLib/UI/Types/UIContext.h
+++ SDLCoreLib/include/SDLCoreLib/UI/Types/UIContext.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <deque>
+#include <vector>
 
 #include <CoreLib/Math/Vector2.h>
 
@@ -34,6 +34,12 @@ namespace SDLCore::UI {
 		float GetWindowScale() const;
 		Vector2 GetWindowSize() const;
 
+		Vector2 GetMousePos() const;
+		Vector2 GetMouseDelta() const;
+		bool GetLeftMouseDown() const;
+		bool GetLeftMouseJustDown() const;
+		bool GetLeftMouseJustUp() const;
+
 		// should be used with HasPressNodeCaptured() to check if a node is Captured
 		uintptr_t GetActiveCapturedPressNode() const;
 		// should be used with HasDragNodeCaptured() to check if a node is Captured
@@ -42,15 +48,22 @@ namespace SDLCore::UI {
 		uintptr_t GetActiveCapturedFocusNode() const;
 
 	private:
-		UIContext() = default;
+		UIContext();
 
-		std::deque<uint16_t> m_lastChildPosition;/*< is the position of the current child inside of last node*/
-		std::deque<UINode*> m_nodeStack;/*< is for creating nodes. if a node is this stack, than those nodes are currently created*/
-		std::deque<UINode*> m_lastNodeStack;
+		std::vector<uint16_t> m_lastChildPosition;/*< is the position of the current child inside of last node*/
+		std::vector<UINode*> m_nodeCreationStack;/*< is for creating nodes. if a node is this stack, than those nodes are currently created*/
+		std::vector<UINode*> m_lastNodeStack;
 		std::shared_ptr<FrameNode> m_rootNode = nullptr;
 		WindowID m_windowID;
 		float m_windowContentScale = 1.0f;
 		Vector2 m_windowSize{ 0.0f, 0.0f };
+
+		uint64_t m_updateInputFrame = 0;
+		Vector2 m_mousePos{ 0.0f, 0.0f };
+		Vector2 m_mouseDelta{ 0.0f, 0.0f };
+		bool m_leftMouseDown = false;
+		bool m_leftMouseJustDown = false;
+		bool m_leftMouseJustUp = false;
 
 		uintptr_t m_pressNodeID = 0;/*< Address can not be 0 */
 		uintptr_t m_dragNodeID = 0;/*< Address can not be 0 */
@@ -58,7 +71,7 @@ namespace SDLCore::UI {
 
 		FrameNode* BeginFrame(uintptr_t id);
 		UIEvent EndFrame();
-
+		
 		template<typename T, typename ...Args>
 		T* AddNode(uintptr_t id, Args&&... args) {
 			if (!m_rootNode) {
@@ -95,6 +108,7 @@ namespace SDLCore::UI {
 		UINode* GetRootNode() const;
 		void SetWindowParams(WindowID id);
 
+		void UpdateInput();
 		static UIEvent* ProcessEvent(UIContext* ctx, UINode* node);
 		static void RenderNodes(UIContext* ctx, UINode* rootNode);
 
