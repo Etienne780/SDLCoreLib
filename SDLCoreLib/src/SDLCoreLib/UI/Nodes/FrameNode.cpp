5d9dc48196e16a6f0d9744a3ca1f798df9affeeb
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2025-12-25 16:36:14 +0100

    feat: text color property

--- SDLCoreLib/src/SDLCoreLib/UI/Nodes/FrameNode.cpp
+++ SDLCoreLib/src/SDLCoreLib/UI/Nodes/FrameNode.cpp
@@ -10,26 +10,6 @@ namespace SDLCore::UI {
 	FrameNode::FrameNode(int childPos, uintptr_t key)
 		: UINode(childPos, key, frameNodeName) {
 	}
-
-	void FrameNode::ApplyStyleCalled(UIContext* ctx, const UIStyleState& styleState) {
-		if (!ctx)
-			return;
-
-		m_backgroundColor.Set(0);
-		styleState.TryGetValue<Vector4>(Properties::backgroundColor, m_backgroundColor);
-
-		m_borderColor.Set(0);
-		styleState.TryGetValue<Vector4>(Properties::borderColor, m_borderColor);
-
-		m_borderWidth = 0;
-		styleState.TryGetValue<float>(Properties::borderWidth, m_borderWidth);
-
-		m_innerBorder = false;
-		styleState.TryGetValue<bool>(Properties::borderInset, m_innerBorder);
-
-		m_useTexture = styleState.TryGetValue<std::shared_ptr<Texture>>(Properties::backgroundTexture, m_texture);
-	}
-
 	void FrameNode::RenderNode(UIContext* ctx) const {
 		namespace RE = SDLCore::Render;
 
@@ -56,5 +36,25 @@ namespace SDLCore::UI {
 	uint32_t FrameNode::GetType() {
 		return Internal::nameToID[frameNodeName];
 	}
+
+	void FrameNode::ApplyStyleCalled(UIContext* ctx, const UIStyleState& styleState) {
+		if (!ctx)
+			return;
+
+		m_backgroundColor.Set(0);
+		styleState.TryGetValue<Vector4>(Properties::backgroundColor, m_backgroundColor);
+
+		m_borderColor.Set(0);
+		styleState.TryGetValue<Vector4>(Properties::borderColor, m_borderColor);
+
+		m_borderWidth = 0;
+		styleState.TryGetValue<float>(Properties::borderWidth, m_borderWidth);
+
+		m_innerBorder = false;
+		styleState.TryGetValue<bool>(Properties::borderInset, m_innerBorder);
+
+		m_useTexture = styleState.TryGetValue<std::shared_ptr<Texture>>(Properties::backgroundTexture, m_texture);
+	}
+
 
 }
